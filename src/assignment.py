43d37ab5b8e778867fa33f9cd2d825ad46cb4a98
eyosias-3 <27hongoe@retsdstudent.ca>
2025-02-19 20:46:41 +0000

    mainn

--- src/assignment.py
+++ src/assignment.py
@@ -13,7 +13,10 @@ def create_player_tag(player_name, player_number):
         create_player_tag("Mario", 123) should return "Mario#123"
     """
     # Your code here
-    pass
+    player_number_string = str(player_number)
+    tag = player_name + "#" + player_number_string
+
+    return tag
 
 def calculate_points_needed(current_score, target_score):
     """
@@ -30,7 +33,8 @@ def calculate_points_needed(current_score, target_score):
         calculate_points_needed(100, 150) should return 50
     """
     # Your code here
-    pass
+    calculate_points_needed = target_score - current_score 
+    return calculate_points_needed
 
 def create_team_roster(team_size, player_symbol):
     """
@@ -47,7 +51,8 @@ def create_team_roster(team_size, player_symbol):
         create_team_roster(3, "🏃") should return "🏃🏃🏃"
     """
     # Your code here
-    pass
+    team = team_size * player_symbol
+    return team
 
 def distribute_powerups(total_powerups, players_count):
     """
@@ -64,4 +69,5 @@ def distribute_powerups(total_powerups, players_count):
         distribute_powerups(10, 3) should return 1 (3 players get 3 each, 1 left over)
     """
     # Your code here
-    pass
+    powerups = total_powerups % players_count
+    return powerups
